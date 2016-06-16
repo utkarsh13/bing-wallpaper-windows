977861ac32766cd0b4ec9d13eb6689ea123e5607
Utkarsh Rastogi <utkarsh13@users.noreply.github.com>
2016-06-16 14:59:52 +0530

    Update setWallpaper.py

--- setWallpaper.py
+++ setWallpaper.py
@@ -5,15 +5,13 @@ Created on Wed Jun 15 20:00:03 2016
 @author: Utkarsh Rastogi
 """
 
-import ctypes
-import win32con
+import os
  
 def setWallpaper(path):
-    # This code is based on the following two links
-    # http://mail.python.org/pipermail/python-win32/2005-January/002893.html
-    # http://code.activestate.com/recipes/435877-change-the-wallpaper-under-windows/
-    cs = ctypes.c_buffer(path)
-    ctypes.windll.user32.SystemParametersInfoA(win32con.SPI_SETDESKWALLPAPER, 0, cs, 0)
+    cmd = 'REG ADD \"HKCU\Control Panel\Desktop\" /v Wallpaper /t REG_SZ /d \"%s\" /f' %path
+    os.system(cmd)
+    os.system('rundll32.exe user32.dll, UpdatePerUserSystemParameters')
+    return
  
 if __name__ == "__main__":
     path = r'C:\Users\Utkarsh Rastogi\Pictures\pic.jpg'
